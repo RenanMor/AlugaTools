d866223a9bf972871f3f675f26b52d0612d3e937
RenanMor <renanm@alunos.utfpr.edu.br>
2026-06-27 12:43:13 -0300

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -3,16 +3,6 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
-// Exclude server-side Node.js code from the Metro bundle.
-// These directories contain mysql2, drizzle-orm, express, net, etc.
-// which are not compatible with React Native / the web bundler.
-config.resolver.blockList = [
-  /.*\/server\/.*/,
-  /.*\/drizzle\/.*/,
-  /.*\/backend\/.*/,
-  /.*\/scripts\/.*/,
-];
-
 module.exports = withNativeWind(config, {
   input: "./global.css",
   // Force write CSS to file system instead of virtual modules
